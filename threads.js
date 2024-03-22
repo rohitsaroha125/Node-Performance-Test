c4327694e1d6d0a30a4d2aab2e0ee9d5f7ddf7c4
Rohit Rohit <rohrohit@WKMZT4D5258C.global.publicisgroupe.net>
2024-03-22 10:10:05 +0530

    Testing thread pools size based on number of cores

--- threads.js
+++ threads.js
@@ -1,3 +1,5 @@
+process.env.UV_THREADPOOL_SIZE = 18;
+
 const crypto = require("crypto");
 
 const startTime = Date.now();
@@ -24,3 +26,51 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
 crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
   console.log("6: ", Date.now() - startTime);
 });
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("7: ", Date.now() - startTime);
+});
+
+crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
+  console.log("8: ", Date.now() - startTime);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("9: ", Date.now() - startTime);
+});
+
+crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
+  console.log("10: ", Date.now() - startTime);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("11: ", Date.now() - startTime);
+});
+
+crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
+  console.log("12: ", Date.now() - startTime);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    console.log("13: ", Date.now() - startTime);
+  });
+  
+  crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
+    console.log("14: ", Date.now() - startTime);
+  });
+  
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    console.log("15: ", Date.now() - startTime);
+  });
+  
+  crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
+    console.log("16: ", Date.now() - startTime);
+  });
+  
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    console.log("17: ", Date.now() - startTime);
+  });
+  
+  crypto.pbkdf2("c", "b", 100000, 512, "sha512", () => {
+    console.log("18: ", Date.now() - startTime);
+  });
